51721975ec95c3dc87d46bd9adb66bfffe3dd38c
Dmitry Dolgushin <razor9714@gmail.com>
2020-09-08 17:22:42 +0500

    поправил код

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import 'react-app-polyfill/ie11'
-import 'react-app-polyfill/stable'
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
